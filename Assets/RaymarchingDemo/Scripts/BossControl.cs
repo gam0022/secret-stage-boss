a627f67e7a4fb43fb9373998e73607ff4658d074
gam0022 <gam0022@gmail.com>
2021-04-02 05:35:10 +0900

    BossControl

---
+++ Assets/RaymarchingDemo/Scripts/BossControl.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.Timeline;
+
+namespace Revision2021
+{
+    public class BossControl : MonoBehaviour, ITimeControl
+    {
+        [SerializeField] Transform target;
+        [SerializeField] Vector3 offset = new Vector3(0, 0, -20);
+
+
+        public void SetTime(double time)
+        {
+            var p = target.position + offset;
+            p.y += 0.1f * Mathf.Sin((float)(Mathf.PI * 2f * time));
+            transform.position = p;
+        }
+
+        public void OnControlTimeStart()
+        {
+        }
+
+        public void OnControlTimeStop()
+        {
+        }
+    }
+}
