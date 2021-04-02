0e3a0216265a1a2bd9f65596f0d7cc24abdb78ae
gam0022 <gam0022@gmail.com>
2021-04-02 13:41:56 +0900

    Wip ShipControl Damage

--- Assets/RaymarchingDemo/Scripts/ShipControl.cs
+++ Assets/RaymarchingDemo/Scripts/ShipControl.cs
@@ -7,15 +7,39 @@ namespace Revision2021
     {
         [SerializeField] Transform shipTransform;
 
+        [SerializeField] Transform shipMeshTransform;
+
+        [SerializeField] float DamegeAnimationSpeed = 2f;
+        [SerializeField] float DamageAnimationIntensity = 4f;
+        [SerializeField] float DamageAnimationFrequently = 10f;
+
         readonly int shipPositionID = Shader.PropertyToID("_ShipPosition");
         readonly int shipDamageBeatID = Shader.PropertyToID("_ShipDamageBeat");
 
         float lastDamageBeat = -999;
 
+        Vector3 PerlinNoise(float t)
+        {
+            float s = 0.1f;
+
+            return new Vector3(
+                2f * (Mathf.PerlinNoise(t, s) - 0.5f),
+                2f * (Mathf.PerlinNoise(t, s * 2) - 0.5f),
+                2f * (Mathf.PerlinNoise(t, s * 3) - 0.5f)
+            );
+        }
+
         public void SetTime(double time)
         {
             Shader.SetGlobalVector(shipPositionID, shipTransform.position);
-            Shader.SetGlobalFloat(shipDamageBeatID, TimelineTimeControl.Beat - lastDamageBeat);
+
+            float shipDamageBeat = TimelineTimeControl.Beat - lastDamageBeat;
+            Shader.SetGlobalFloat(shipDamageBeatID, shipDamageBeat);
+
+            if (shipDamageBeat > 0)
+            {
+                shipMeshTransform.localPosition = DamageAnimationIntensity * Mathf.Exp(-shipDamageBeat * DamegeAnimationSpeed) * PerlinNoise((float)time * DamageAnimationFrequently);
+            }
         }
 
         public void OnControlTimeStart()
